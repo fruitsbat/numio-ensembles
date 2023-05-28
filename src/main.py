27f1d8701bcdb192b64cc5b049c77f5225f30fa1
Zoe Oosting <oosting@wr.informatik.uni-hamburg.de>
2023-05-28 20:25:08 +0200

    sort commands

--- src/main.py
+++ src/main.py
@@ -4,16 +4,25 @@ import time
 import typer
 import logging
 from rich.logging import RichHandler
+from click import Context
 from config import MODE, LOGLEVEL
 from typing_extensions import Annotated
+from typing import Iterable
 from rich.progress import track
+from typer.core import TyperGroup
 
-app = typer.Typer()
+
+# return commands in a more reasonable order than alphabetical
+class OrderCommands(TyperGroup):
+    def list_commands(self, ctx: Context):
+        return list(self.commands)  # return commands in order of appearance
 
 
-# run a test
+app = typer.Typer(cls=OrderCommands)
+
+
 @app.command()
-def run(
+def simple(
     load: Annotated[
         MODE,
         typer.Option(
@@ -23,7 +32,29 @@ def run(
         ),
     ] = MODE.EMPTY.value
 ):
+    """
+    this mode offers preconfigured options.
+    use this to run a set of sensible, preconfigured benchmarks.
+    """
     print(load)
+
+
+@app.command()
+def advanced():
+    """
+    this mode offers some options.
+    use this if you would like to tweak the script a bit.
+    """
+    pass
+
+
+@app.command()
+def expert():
+    """
+    in this mode, the script tries to get out of your way!
+    use this if you want to configure everything yourself.
+    """
+    pass
 
 
 @app.callback()
@@ -31,8 +62,14 @@ def main(
     loglevel: Annotated[
         LOGLEVEL,
         typer.Option("--loglevel", "-l", help="adjust chattyness of app"),
-    ] = LOGLEVEL.INFO
+    ] = LOGLEVEL.INFO.value
 ):
+    """
+    this is a script designed to help you quickly run numio benchmarks.
+    it tries to simulate realistic test conditions by creating disk and network load.
+    these are shared resources, so performance issues related to them would be hard to find using an isolated program.
+
+    """
     loglevel.init_logging()
 
 
